fea55ef61904d1568be237a36c59e144123c56e7
suleiman010 <suleimananwarmubarak@gmail.com>
2025-09-14 18:35:01 +0300

    feat: add profileInfo section to the navbar

--- frontend/note-app/src/components/NavBar/NavBar.jsx
+++ frontend/note-app/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import ProfileInfo from "../Cards/ProfileInfo";
 
 const NavBar = () => {
+  const naviage = useNavigate();
+  const onLogout = () => {
+    naviage("/login");
+  };
+
   return (
     <header className="bg-white flex items-center justify-between py-6 px-2 drop-shadow">
       <h2 className="text-black py-2 font-medium">NoterTaker</h2>
+
+      <ProfileInfo onLogout={onLogout} />
     </header>
   );
 };
